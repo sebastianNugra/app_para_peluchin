71c9d7a920dafccc3fdba6be67ab16c7d6b4a7f8
QUEZADA MENDIA STEVEN DAVID <steven.quezada@est.ucacue.edu.ec>
2026-03-31 21:24:44 -0500

    Primer commit

---
+++ src/main/java/com/example/hombre_maquina/HombreMaquinaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hombre_maquina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HombreMaquinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HombreMaquinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hombre_maquina/HombreMaquinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hombre_maquina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HombreMaquinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
